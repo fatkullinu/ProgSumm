b8f42503f3f3d6f8265b199fe47447df64910ce4
Usman <fatkullinu@hotmail.com>
2024-05-21 14:32:54 +0500

    Modify input in main program

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,8 @@
 ﻿void Main()
 {
 // Запрашиваем массив строк
-Console.WriteLine("Вводите элементов массива через запятую "например: Hello, 2, world,:-)" :");
-string input = Console.ReadLine();
+Console.WriteLine("Вводите элементов массива через запятую например: Hello, 2, world,:-) :");
+string? input = Console.ReadLine();
 
 // Разбиваем введенную строку на массив строк
 string[] array = input.Split(',');
