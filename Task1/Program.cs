945b9590e3e95923a26b3c7f71c39f07b4353ce1
Usman <fatkullinu@hotmail.com>
2024-05-21 14:43:40 +0500

    Add method FilterArray

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,18 +1,46 @@
 ﻿void Main()
 {
-// Запрашиваем массив строк
-Console.WriteLine("Вводите элементов массива через запятую например: Hello, 2, world,:-) :");
-string? input = Console.ReadLine();
+    // Запрашиваем массив строк
+    Console.WriteLine("Вводите элементов массива через запятую например: Hello, 2, world,:-) :");
+    string? input = Console.ReadLine();
 
-// Разбиваем введенную строку на массив строк
-string[] array = input!.Split(',');
+    // Разбиваем введенную строку на массив строк
+    string[] array = input!.Split(',');
 
-// Формируем новый массив из строк, длина которых меньше или равна 3 символам
-string[] newArray = FilterArray(array);
+    // Формируем новый массив из строк, длина которых меньше чем 4 символа
+    string[] newArray = FilterArray(array);
 
-// Выводим новый массив на экран
-Console.WriteLine("Новый массив:");
-PrintArray(newArray);
+    // Выводим новый массив на экран
+    Console.WriteLine("Новый массив:");
+    PrintArray(newArray);
 }
+
+string[] FilterArray(string[] array)
+{
+    // Подсчитываем количество строк, в которых менее чем 4 символа
+    int count = 0;
+    foreach (string item in array)
+    {
+        if (item.Trim().Length < 4)
+        {
+            count++;
+        }
+    }
+
+    // Создаем новый массив с подходящими строками
+    string[] newArray = new string[count];
+    int index = 0;
+    foreach (string item in array)
+    {
+        if (item.Trim().Length <= 3)
+        {
+            newArray[index] = item.Trim();
+            index++;
+        }
+    }
+
+    return newArray;
+}
+
 
 Main();
