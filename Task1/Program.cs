ab75aa3fd741ff4858e328d4df91c49e0bdc7ad8
Usman <fatkullinu@hotmail.com>
2024-05-21 14:26:08 +0500

    Add main programm

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+// Запрашиваем массив строк
+Console.WriteLine("Вводите элементов массива через запятую "например: Hello, 2, world,:-)" :");
+string input = Console.ReadLine();
+
+// Разбиваем введенную строку на массив строк
+string[] array = input.Split(',');
+
+// Формируем новый массив из строк, длина которых меньше или равна 3 символам
+string[] newArray = FilterArray(array);
+
+// Выводим новый массив на экран
+Console.WriteLine("Новый массив:");
+PrintArray(newArray);
+}
+
+Main();
